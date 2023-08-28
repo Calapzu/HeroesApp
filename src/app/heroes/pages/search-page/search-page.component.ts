61cb43a6691f3e5056c1b72a473b53c0fa131a5d
Calapzu <yamsoncalapzu@gmail.com>
2023-08-28 18:00:24 -0500

    Agregar Heroe - HTML

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-search-page',
@@ -10,5 +15,29 @@ import { FormControl } from '@angular/forms';
 export class SearchPageComponent {
 
   public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  constructor( private heroesService: HeroesService){}
+
+  searchHero(){
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions( value )
+      .subscribe( heroes => this.heroes = heroes);
+  }
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent ):void {
+    if ( !event.option.value ) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+
+    this.selectedHero = hero;
+    console.log( event.option.value );
+  }
 
 }
