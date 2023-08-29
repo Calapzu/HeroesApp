84c156b0b4a025725042785fafa802ded4b2709c
Calapzu <yamsoncalapzu@gmail.com>
2023-08-29 01:00:33 -0500

    Material Dialogs

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { switchMap } from 'rxjs';
 
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
+
 
 @Component({
   selector: 'app-new-page',
@@ -35,7 +40,8 @@ export class NewPageComponent implements OnInit{
     private heroesService: HeroesService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog,
   ){}
 
 
@@ -81,6 +87,23 @@ export class NewPageComponent implements OnInit{
       this.showSnackbar(`${ hero.superhero } created!`);
     })
     //this.heroesService.updateHero( this.heroForm.value )
+  }
+
+  onDeleteHero() {
+
+    if ( !this.currentHero.id ) throw Error('Hero id is required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if ( !result ) return;
+
+      this.heroesService.deleteHeroById( this.currentHero.id );
+      this.router.navigate(['/heroes'])
+    });
+
   }
 
   showSnackbar( message: string ):void {
